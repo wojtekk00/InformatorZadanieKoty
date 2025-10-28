a2693db7109b870bdf0a7da211dbe54a2e6787b0
unknown <wojtekrocker12@gmail.com>
2025-10-28 11:01:52 +0100

    funkcje dla przyciskow i switcha

--- app/src/main/java/com/example/zadadadankozinformatora/MainActivity.java
+++ app/src/main/java/com/example/zadadadankozinformatora/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.zadadadankozinformatora;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +18,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    LinearLayout main;
     ImageView imageViewZdjeciaKotow;
     Button buttonPrev;
     Button buttonNext;
     EditText editTextNumerObrazu;
     Switch switchTloNiebieskie;
+    int wyswietlaneZdjecie = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +45,56 @@ public class MainActivity extends AppCompatActivity {
         buttonNext = findViewById(R.id.buttonNext);
         editTextNumerObrazu = findViewById(R.id.editTextNumerObrazu);
         switchTloNiebieskie = findViewById(R.id.switchTloNiebieskie);
+        main = findViewById(R.id.main);
 
+        ArrayList<Integer> zdjeciaKotow = new ArrayList<>();
+        zdjeciaKotow.add(R.drawable.kot1);
+        zdjeciaKotow.add(R.drawable.kot2);
+        zdjeciaKotow.add(R.drawable.kot3);
 
+        imageViewZdjeciaKotow.setImageResource(zdjeciaKotow.get(0));
+
+        switchTloNiebieskie.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (switchTloNiebieskie.isChecked()){
+                    main.setBackgroundColor(Color.parseColor("#1565c0"));
+                }
+                else{
+                    main.setBackgroundColor(Color.parseColor("#00796B"));
+                }
+            }
+        });
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (wyswietlaneZdjecie < 2){
+                    wyswietlaneZdjecie ++;
+                } else if (wyswietlaneZdjecie == 2) {
+                    wyswietlaneZdjecie = 0;
+                }
+                imageViewZdjeciaKotow.setImageResource(zdjeciaKotow.get(wyswietlaneZdjecie));
+            }
+        });
+
+        buttonPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (wyswietlaneZdjecie > 0){
+                    wyswietlaneZdjecie --;
+                } else if (wyswietlaneZdjecie == 0) {
+                    wyswietlaneZdjecie = 2;
+                }
+                imageViewZdjeciaKotow.setImageResource(zdjeciaKotow.get(wyswietlaneZdjecie));
+            }
+        });
+
+        editTextNumerObrazu.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+            }
+        });
     }
 }
